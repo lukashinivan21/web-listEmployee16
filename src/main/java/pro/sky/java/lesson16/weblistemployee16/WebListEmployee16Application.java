ecb981b3db3550f1099be34bf9d957a287642d8f
Ivan <uvanes21@yandex.ru>
2022-02-26 07:39:53 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/java/lesson16/weblistemployee16/WebListEmployee16Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.lesson16.weblistemployee16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebListEmployee16Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebListEmployee16Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/lesson16/weblistemployee16/WebListEmployee16ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.lesson16.weblistemployee16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebListEmployee16ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
